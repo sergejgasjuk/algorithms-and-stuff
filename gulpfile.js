2304b86d735819cd67c0296401cd4ae387be4d1d
Serhiy Hasiuk <Serhiy.Hasiuk@symphony-solutions.eu>
2015-11-09 19:29:37 +0200

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,35 @@ gulp.task('sorting-html', function() {
     .pipe(gulp.dest('./sorting-algorithms/build'))
 });
 
-gulp.task('sorting-algorithms', ['sorting-js', 'sorting-html']);
-
 gulp.task('sorting-algorithms-watch', function(){
   gulp.watch('./sorting-algorithms/src/**/*', ['sorting-html', 'sorting-js']);
 });
 
+gulp.task('sorting-algorithms-build', ['sorting-js', 'sorting-html'], function(){
+  gulp.start('sorting-algorithms-watch');
+});
+
+// Binary Search Tree
+gulp.task('BST-js', function() {
+  return browserify('./binary-search-tree/src/js/main.js')
+    //.transform(babelify)
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./binary-search-tree/build/js/'));
+});
+
+gulp.task('BST-html', function() {
+  gulp.src('./binary-search-tree/src/index.html')
+    .pipe(gulp.dest('./binary-search-tree/build'))
+});
+
+gulp.task('BST-watch', function(){
+  gulp.watch('./binary-search-tree/src/**/*', ['sorting-html', 'sorting-js']);
+});
+
+gulp.task('BST-build', ['BST-js', 'BST-html'], function(){
+  gulp.start('BST-watch');
+});
+
 // Default Task
-gulp.task('default', ['sorting-algorithms']);
+gulp.task('default', []);
