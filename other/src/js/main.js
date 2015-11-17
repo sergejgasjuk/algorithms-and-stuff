c7f498ac70d03c0752671e2a59e0142b417583bb
Serhiy Hasiuk <Serhiy.Hasiuk@symphony-solutions.eu>
2015-11-17 12:11:08 +0200

    added fibo 2

--- other/src/js/main.js
+++ other/src/js/main.js
@@ -1,5 +1,23 @@
+import {timingDecorator as bench} from "helpers";
 import * as GCD from "./stuff/greatest-common-divisor";
 import * as fibo from "./stuff/fibonacci";
 
-console.log(fibo.fibo_iterative(7));
+let fiboNum = 41; // 165580141  
+
+let fibRecursive = bench(fibo.fibo_recursive),
+    fibIterative = bench(fibo.fibo_iterative),
+    fibBadRecursive = bench(fibo.fibo_badRecursion);
+
+let fibRecursiveResult = fibRecursive(fiboNum),
+    fibIterativeResult = fibIterative(fiboNum),
+    fibBadRecursiveResult = fibBadRecursive(fiboNum);
+
+console.log(`fibo recursive: ${fibRecursiveResult.result} \n`,
+  `fibo recursive: ${fibRecursiveResult.endTime}`);
+
+console.log(`fibo iterative: ${fibIterativeResult.result} \n`,
+  `fibo recursive: ${fibIterativeResult.endTime}`);
+
+console.log(`fibo bad recursion: ${fibBadRecursiveResult.result} \n`,
+  `fibo bad recursion: ${fibBadRecursiveResult.endTime}`);
 
--- other/src/js/stuff/fibonacci.js
+++ other/src/js/stuff/fibonacci.js
@@ -3,13 +3,9 @@ export function fibo_iterative(n) {
     return false;
   }
   
-  if (n === 0) {
-    return 0;
+  if (n ===  0 || n === 1) {
+    return n;
   }
-  
-  if (n === 1) {
-    return 1;
-  }  
   
   let n1 = 0;
   let n2 = 1;
@@ -25,3 +21,30 @@ export function fibo_iterative(n) {
   return n1;
 }
 
+export function fibo_badRecursion(n) {
+  if (n < 0) {
+    return false;
+  }
+
+  if (n ===  0 || n === 1) {
+    return n;
+  }
+  
+  return fibo_badRecursion(n - 1) + fibo_badRecursion(n - 2);
+}
+
+export function fibo_recursive(n) {
+  let cache = {0: 0, 1: 1};
+  
+  return (function fibo(n) {
+    if (n < 0) {
+      return false;
+    }
+    
+    if (!cache.hasOwnProperty(n)) {
+      return cache[n] = fibo(n - 1) + fibo(n - 2);
+    }
+    
+    return cache[n];
+  })(n);
+}
